8761eb5559ef59d29cda5710737afb39c59b244f
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-04-02 19:35:45 -0400

    fix pylint

--- tests/test_IEX_API.py
+++ tests/test_IEX_API.py
@@ -9,13 +9,10 @@ def test_get_IEX_API(requests_mock):
     requests_mock.get(
         url,
         json={
-            "AAPL":{"quote":{"symbol":"AAPL","companyName":"Apple Inc.","latestPrice":189.95}},
-            "FB":{"quote":{"symbol":"FB","companyName":"Facebook Inc.","latestPrice":166.62}}
+            "AAPL":{"quote":{"symbol":"AAPL", "companyName":"Apple Inc.", "latestPrice":189.95}},
+            "FB":{"quote":{"symbol":"FB", "companyName":"Facebook Inc.", "latestPrice":166.62}}
             }
         )
-    expected = [
-            ('AAPL'),
-            ('FB')
-        ]
+    expected = [('AAPL'),('FB')]
 
     assert get_symbol_list_IEX_API(url) == expected
