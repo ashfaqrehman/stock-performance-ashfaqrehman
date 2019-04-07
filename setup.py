41c808261e7c7549e16d249d1e30e3c6c54a5b14
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-04-07 18:43:41 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description='The program will read a CSV file containing portfolio data and generate a new CSV report using using live market price on IEX API.',
     packages=['portfolio'],
     entry_points={
-        'console_scripts':['portfolio_report=portfolio_report:main']
+        'console_scripts':['portfolio_report=portfolio.portfolio_report:main']
     },
     install_requires=[
         "requests"
