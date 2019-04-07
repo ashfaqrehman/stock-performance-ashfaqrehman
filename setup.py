cdaa187674997f7d8eb145795798f5a292b990f1
Ashfaq Rehman <rehman@sheridancollege.ca>
2019-04-07 19:22:16 -0400

    fix pylint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version='0.0.1',
     author='Ash',
     description='The program will read a CSV file containing portfolio data and'
-     'generate a new CSV report using using live market price on IEX API.',
+    'generate a new CSV report using using live market price on IEX API.',
     packages=['portfolio'],
     entry_points={
         'console_scripts':['portfolio_report=portfolio.portfolio_report:main']
